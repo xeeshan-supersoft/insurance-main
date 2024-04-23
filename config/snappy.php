2793fe4139a3844c6b65c16494156c341f26a7ea
Attiq Arian <attiq.arain.arabsea@gmail.com>
2024-04-23 16:51:25 +0500

    Sc

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
 
     // 'pdf' => [
     //     'enabled' => true,
-    //     'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+    //     'binary'  => '/home/youexcel/public_html/public/insurance/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64',
     //     'timeout' => false,
     //     'options' => [],
     //     'env'     => [],
@@ -43,7 +43,7 @@ return [
 
     // 'image' => [
     //     'enabled' => true,
-    //     'binary'  => base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64'),
+    //     'binary'  => '/home/youexcel/public_html/public/insurance/vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64',
     //     'timeout' => false,
     //     'options' => [],
     //     'env'     => [],
