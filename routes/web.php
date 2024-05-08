6bb84c7f012dca16bf67ad67e8286ccaaa40bbad
Attiq Arian <attiq.arain.arabsea@gmail.com>
2024-05-08 21:24:52 +0500

    Cha

--- routes/web.php
+++ routes/web.php
@@ -56,13 +56,9 @@ Route::get('/accord', function () {
   return view('fromdrop');
 });
 
-Route::get('/accord', function () {
-  return view('fromdrop');
+Route::get('/form3', function () {
+  return view('agent.form3');
 });
-
-// Route::get('/pdf2', function () {
-//   return view('agent.form_pdf2');
-// });
 
 Route::get('/insurSearch', [ssc::class, 'selectSearch'])->name('insurSearch');
 
