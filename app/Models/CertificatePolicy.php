54b29e3c99c757c2b720eb9650047dfa5f83d96d
hatim <laravel.supersoft@gmail.com>
2024-05-09 13:11:38 +0500

    migration work

--- app/Models/CertificatePolicy.php
+++ app/Models/CertificatePolicy.php
@@ -12,7 +12,7 @@ class CertificatePolicy extends Model
     protected $table = 'certificate_policies';
 
     protected $fillable = [
-        'certificate_id', 'policy_type_id', 'policy_id', 'policy_deductible',
+        'certificate_id', 'policy_type_id','policy_amount', 'policy_id', 'policy_deductible',
         'policy_retention', 'is_policy_checked', 'is_risk_retention_insured',
         'is_actual_cash_value', 'insurance_provider_id', 'insurance_provider_code',
         'policy_number', 'issue_date', 'start_date', 'expiry_date',
