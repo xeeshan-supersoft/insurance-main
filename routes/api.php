8f2adb51e3a13164f8294145c55e6b53df7e8ac5
hatim <laravel.supersoft@gmail.com>
2024-06-20 12:39:20 +0500

    hatim

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post("/login",[App\Http\Controllers\AuthController::class, 'login']);
 
 
 
