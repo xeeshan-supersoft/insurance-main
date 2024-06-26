1a85207b9cb5dc13be22aa54dae9adc3cf004587
Attiq Arian <attiq.arain.arabsea@gmail.com>
2024-06-26 19:25:08 +0500

    ccc

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->enum('role', ['admin', 'agent', 'shipper', 'truck_driver', 'freight_driver']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('status')->default(0);
+            $table->integer('status')->nullable()->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
